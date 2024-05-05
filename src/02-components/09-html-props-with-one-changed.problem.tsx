fb4c7c3c99aff14735545cbbe7adeb931d24b4e3
didoslavov <didkoslawow@gmail.com>
2024-05-05 23:08:07 +0300

    solve task 9

--- src/02-components/09-html-props-with-one-changed.problem.tsx
+++ src/02-components/09-html-props-with-one-changed.problem.tsx
@@ -1,27 +1,23 @@
-import { ComponentProps } from "react";
-import { Equal, Expect } from "../helpers/type-utils";
+import { ChangeEventHandler, ComponentProps } from 'react';
+import { Equal, Expect } from '../helpers/type-utils';
 
-export const Input = (
-  props: ComponentProps<"input"> & { onChange: (value: string) => void }
-) => {
-  return (
-    <input
-      {...props}
-      onChange={(e) => {
-        props.onChange(e.target.value);
-      }}
-    ></input>
-  );
+export const Input = (props: Omit<ComponentProps<'input'>, 'onChange'> & { onChange: (value: string) => void }) => {
+    return (
+        <input
+            {...props}
+            onChange={(e) => {
+                props.onChange(e.target.value);
+            }}></input>
+    );
 };
 
 const Parent = () => {
-  return (
-    <Input
-      onChange={(e) => {
-        console.log(e);
+    return (
+        <Input
+            onChange={(e) => {
+                console.log(e);
 
-        type test = Expect<Equal<typeof e, string>>;
-      }}
-    ></Input>
-  );
+                type test = Expect<Equal<typeof e, string>>;
+            }}></Input>
+    );
 };
