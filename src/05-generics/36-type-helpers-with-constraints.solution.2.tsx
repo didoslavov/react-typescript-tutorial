618aee40842bd4b86c0a308af53af485789cb93c
didoslavov <didkoslawow@gmail.com>
2024-05-07 02:48:24 +0300

    solve task 36

--- src/05-generics/36-type-helpers-with-constraints.solution.2.tsx
+++ src/05-generics/36-type-helpers-with-constraints.solution.2.tsx
@@ -1,19 +1,17 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 type AllOrNothing<T extends Record<string, any>> = T | ToUndefinedObject<T>;
 
-type ToUndefinedObject<T extends Record<string, any>> = Partial<
-  Record<keyof T, undefined>
->;
+type ToUndefinedObject<T extends Record<string, any>> = Partial<Record<keyof T, undefined>>;
 
 type tests = [
-  // @ts-expect-error
-  AllOrNothing<string>,
-  // @ts-expect-error
-  AllOrNothing<number>,
-  // @ts-expect-error
-  AllOrNothing<undefined>,
-  Expect<Equal<AllOrNothing<{ a: string }>, { a: string } | { a?: undefined }>>
+    // @ts-expect-error
+    AllOrNothing<string>,
+    // @ts-expect-error
+    AllOrNothing<number>,
+    // @ts-expect-error
+    AllOrNothing<undefined>,
+    Expect<Equal<AllOrNothing<{ a: string }>, { a: string } | { a?: undefined }>>
 ];
 
 // Note that this isn't perfect! Try AllOrNothing<string[]>
