f2306f70690bbceca0d2acae5cc4a336b4e91444
didoslavov <didkoslawow@gmail.com>
2024-05-07 02:34:36 +0300

    solve task 35

--- src/05-generics/35-type-helpers-2.problem.tsx
+++ src/05-generics/35-type-helpers-2.problem.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler } from "react";
+import { ChangeEventHandler } from 'react';
 
 /**
  * It would be really nice to refactor this so that it's
@@ -8,41 +8,38 @@ import { ChangeEventHandler } from "react";
  * and returns it along with a union with all of its
  * keys turned to undefined.
  */
-export type InputProps = (
-  | {
-      value: string;
-      onChange: ChangeEventHandler;
-    }
-  | {
-      value?: undefined;
-      onChange?: undefined;
-    }
-) & {
-  label: string;
+type ReusableType<T> = T | UndefinedType<T>;
+type UndefinedType<T> = Partial<Record<keyof T, undefined>>;
+
+export type InputProps = ReusableType<{
+    value: string;
+    onChange: ChangeEventHandler;
+}> & {
+    label: string;
 };
 
 export const Input = ({ label, ...props }: InputProps) => {
-  return (
-    <div>
-      <label>
-        {label}
-        <input {...props} />
-      </label>
-    </div>
-  );
+    return (
+        <div>
+            <label>
+                {label}
+                <input {...props} />
+            </label>
+        </div>
+    );
 };
 
 export const Test = () => {
-  return (
-    <div>
-      <Input label="Greeting" value="Hello" onChange={() => {}} />
-      <Input label="Greeting" />
+    return (
+        <div>
+            <Input label="Greeting" value="Hello" onChange={() => {}} />
+            <Input label="Greeting" />
 
-      {/* @ts-expect-error */}
-      <Input label="Greeting" value="Hello" />
+            {/* @ts-expect-error */}
+            <Input label="Greeting" value="Hello" />
 
-      {/* @ts-expect-error */}
-      <Input label="Greeting" onChange={() => {}} />
-    </div>
-  );
+            {/* @ts-expect-error */}
+            <Input label="Greeting" onChange={() => {}} />
+        </div>
+    );
 };
