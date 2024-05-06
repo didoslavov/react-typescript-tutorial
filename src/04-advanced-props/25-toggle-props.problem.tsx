80095e05f4fa8547447485798d443492ce969ac0
didoslavov <didkoslawow@gmail.com>
2024-05-06 18:48:26 +0300

    solve task 25

--- src/04-advanced-props/25-toggle-props.problem.tsx
+++ src/04-advanced-props/25-toggle-props.problem.tsx
@@ -11,34 +11,34 @@
  * Hint - you'll need a discriminated union!
  */
 
-type EmbeddedPlaygroundProps = {
-  useStackblitz?: boolean;
-  stackblitzId?: string;
-  codeSandboxId?: string;
-};
+type EmbeddedPlaygroundProps =
+    | {
+          useStackblitz?: false;
+          codeSandboxId: string;
+      }
+    | {
+          useStackblitz: true;
+          stackblitzId: string;
+      };
 
 const EmbeddedPlayground = (props: EmbeddedPlaygroundProps) => {
-  if (props.useStackblitz) {
-    return (
-      <iframe
-        src={`https://stackblitz.com/edit/${props.stackblitzId}?embed=1`}
-      />
-    );
-  }
+    if (props.useStackblitz) {
+        return <iframe src={`https://stackblitz.com/edit/${props.stackblitzId}?embed=1`} />;
+    }
 
-  return <iframe src={`https://codesandbox.io/embed/${props.codeSandboxId}`} />;
+    return <iframe src={`https://codesandbox.io/embed/${props.codeSandboxId}`} />;
 };
 
 <>
-  <EmbeddedPlayground useStackblitz stackblitzId="my-stackblitz-id" />
-  <EmbeddedPlayground codeSandboxId="my-codesandbox-id" />
+    <EmbeddedPlayground useStackblitz stackblitzId="my-stackblitz-id" />
+    <EmbeddedPlayground codeSandboxId="my-codesandbox-id" />
 
-  <EmbeddedPlayground
-    useStackblitz
-    // @ts-expect-error
-    codeSandboxId="my-codesandbox-id"
-  />
+    <EmbeddedPlayground
+        useStackblitz
+        // @ts-expect-error
+        codeSandboxId="my-codesandbox-id"
+    />
 
-  {/* @ts-expect-error */}
-  <EmbeddedPlayground stackblitzId="my-stackblitz-id" />
+    {/* @ts-expect-error */}
+    <EmbeddedPlayground stackblitzId="my-stackblitz-id" />
 </>;
