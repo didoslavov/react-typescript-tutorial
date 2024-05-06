056fc88f8cf271fa76ae3d9362a94ea2d5291e7e
didoslavov <didkoslawow@gmail.com>
2024-05-07 01:38:44 +0300

    solve task 31

--- src/04-advanced-props/31-as-const.problem.ts
+++ src/04-advanced-props/31-as-const.problem.ts
@@ -1,15 +1,12 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 const BACKEND_TO_FRONTEND_STATUS_MAP = {
-  0: "pending",
-  1: "success",
-  2: "error",
-};
+    0: 'pending',
+    1: 'success',
+    2: 'error',
+} as const;
 
-type BackendStatus = unknown;
-type FrontendStatus = unknown;
+type BackendStatus = keyof typeof BACKEND_TO_FRONTEND_STATUS_MAP;
+type FrontendStatus = (typeof BACKEND_TO_FRONTEND_STATUS_MAP)[keyof typeof BACKEND_TO_FRONTEND_STATUS_MAP];
 
-type test = [
-  Expect<Equal<BackendStatus, 0 | 1 | 2>>,
-  Expect<Equal<FrontendStatus, "pending" | "success" | "error">>
-];
+type test = [Expect<Equal<BackendStatus, 0 | 1 | 2>>, Expect<Equal<FrontendStatus, 'pending' | 'success' | 'error'>>];
