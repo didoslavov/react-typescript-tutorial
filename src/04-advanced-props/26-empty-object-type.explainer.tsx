f230f142ee3c6277bf1579f7a133b04db46d9a26
didoslavov <didkoslawow@gmail.com>
2024-05-06 18:52:42 +0300

    solve task 26

--- src/04-advanced-props/26-empty-object-type.explainer.tsx
+++ src/04-advanced-props/26-empty-object-type.explainer.tsx
@@ -1,15 +1,26 @@
-const Component = (props: { config: {} }) => {
-  return <div />;
+const Component = (props: {
+    config: {
+        foo: string;
+        whatever: {
+            test: number;
+            test2: string;
+        };
+    };
+}) => {
+    return <div />;
 };
 
 /**
  * Why can I pass _anything_ to config?
  */
 <>
-  <Component
-    config={{
-      foo: "bar",
-      whatever: {},
-    }}
-  />
+    <Component
+        config={{
+            foo: 'bar',
+            whatever: {
+                test: 1,
+                test2: 'test',
+            },
+        }}
+    />
 </>;
