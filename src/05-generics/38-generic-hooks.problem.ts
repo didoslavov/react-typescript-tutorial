2e8ee153855e613fb971fe490e1522be4449e0f2
didoslavov <didkoslawow@gmail.com>
2024-05-07 04:27:51 +0300

    solve task 38

--- src/05-generics/38-generic-hooks.problem.ts
+++ src/05-generics/38-generic-hooks.problem.ts
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Equal, Expect } from "../helpers/type-utils";
+import { useState } from 'react';
+import { Equal, Expect } from '../helpers/type-utils';
 
 /**
  * 1. In this exercise, we want to create a version of the useState
@@ -8,30 +8,25 @@ import { Equal, Expect } from "../helpers/type-utils";
  *
  * There are _many_ different solutions - but they all involve generics.
  */
-export const useStateAsObject = (initial: any) => {
-  const [value, set] = useState(initial);
+export const useStateAsObject = <T>(initial: T) => {
+    const [value, set] = useState(initial);
 
-  return {
-    value,
-    set,
-  };
+    return {
+        value,
+        set,
+    };
 };
 
-const example = useStateAsObject({ name: "Matt" });
+const example = useStateAsObject({ name: 'Matt' });
 
 type ExampleTests = [
-  Expect<Equal<typeof example.value, { name: string }>>,
-  Expect<
-    Equal<
-      typeof example.set,
-      React.Dispatch<React.SetStateAction<{ name: string }>>
-    >
-  >,
+    Expect<Equal<typeof example.value, { name: string }>>,
+    Expect<Equal<typeof example.set, React.Dispatch<React.SetStateAction<{ name: string }>>>>
 ];
 
 const num = useStateAsObject(2);
 
 type NumTests = [
-  Expect<Equal<typeof num.value, number>>,
-  Expect<Equal<typeof num.set, React.Dispatch<React.SetStateAction<number>>>>,
+    Expect<Equal<typeof num.value, number>>,
+    Expect<Equal<typeof num.set, React.Dispatch<React.SetStateAction<number>>>>
 ];
