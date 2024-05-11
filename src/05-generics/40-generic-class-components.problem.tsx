4427ddc312d2214991fdfed6e82fc23cac11042d
didoslavov <didkoslawow@gmail.com>
2024-05-11 04:57:22 +0300

    solve task 40

--- src/05-generics/40-generic-class-components.problem.tsx
+++ src/05-generics/40-generic-class-components.problem.tsx
@@ -1,50 +1,49 @@
-import React, { ReactNode } from "react";
-import { Equal, Expect } from "../helpers/type-utils";
+import React, { ReactNode } from 'react';
+import { Equal, Expect } from '../helpers/type-utils';
 
-interface TableProps {
-  rows: any[];
-  renderRow: (row: any) => ReactNode;
+interface TableProps<T> {
+    rows: T[];
+    renderRow: (row: T) => ReactNode;
 }
 
-export class Table extends React.Component<TableProps> {
-  render(): ReactNode {
-    return (
-      <table>
-        <tbody>
-          {this.props.rows.map((row) => (
-            <tr>{this.props.renderRow(row)}</tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
+export class Table<T> extends React.Component<TableProps<T>> {
+    render(): ReactNode {
+        return (
+            <table>
+                <tbody>
+                    {this.props.rows.map((row) => (
+                        <tr>{this.props.renderRow(row)}</tr>
+                    ))}
+                </tbody>
+            </table>
+        );
+    }
 }
 
 const data = [
-  {
-    id: 1,
-    name: "John",
-  },
+    {
+        id: 1,
+        name: 'John',
+    },
 ];
 
 export const Parent = () => {
-  return (
-    <div>
-      <Table rows={data} renderRow={(row) => <td>{row.name}</td>} />
-      <Table
-        rows={data}
-        renderRow={(row) => {
-          type test = Expect<Equal<typeof row, { id: number; name: string }>>;
-          return (
-            <td>
-              {
-                // @ts-expect-error
-                row.doesNotExist
-              }
-            </td>
-          );
-        }}
-      ></Table>
-    </div>
-  );
+    return (
+        <div>
+            <Table rows={data} renderRow={(row) => <td>{row.name}</td>} />
+            <Table
+                rows={data}
+                renderRow={(row) => {
+                    type test = Expect<Equal<typeof row, { id: number; name: string }>>;
+                    return (
+                        <td>
+                            {
+                                // @ts-expect-error
+                                row.doesNotExist
+                            }
+                        </td>
+                    );
+                }}></Table>
+        </div>
+    );
 };
