e41d7a1c58bd0a0354ccd9eabdf98e9997cca64b
didoslavov <didkoslawow@gmail.com>
2024-05-12 04:05:51 +0300

    solve taks 43

--- src/05-generics/43-inference-flow-with-constraints.problem.ts
+++ src/05-generics/43-inference-flow-with-constraints.problem.ts
@@ -1,74 +1,74 @@
-import { createUser } from "fake-external-lib";
-import { useState } from "react";
-import { Equal, Expect } from "../helpers/type-utils";
+import { createUser } from 'fake-external-lib';
+import { useState } from 'react';
+import { Equal, Expect } from '../helpers/type-utils';
 
-type Mutation = (...args: any[]) => Promise<any>;
+type Mutation<T extends any[], K> = (...args: T) => Promise<K>;
 
-interface UseMutationReturn {
-  mutate: Mutation;
-  isLoading: boolean;
+interface UseMutationReturn<T extends any[], K> {
+    mutate: Mutation<T, K>;
+    isLoading: boolean;
 }
 
-interface UseMutationOptions {
-  mutation: Mutation;
+interface UseMutationOptions<T extends any[], K> {
+    mutation: Mutation<T, K>;
 }
 
-export const useMutation = (opts: UseMutationOptions): UseMutationReturn => {
-  const [isLoading, setIsLoading] = useState(false);
+export const useMutation = <T extends any[], K>(opts: UseMutationOptions<T, K>): UseMutationReturn<T, K> => {
+    const [isLoading, setIsLoading] = useState(false);
 
-  return {
-    mutate: async (...args) => {
-      setIsLoading(true);
+    return {
+        mutate: async (...args) => {
+            setIsLoading(true);
 
-      try {
-        const result = await opts.mutation(...args);
-        return result;
-      } catch (e) {
-        throw e;
-      } finally {
-        setIsLoading(false);
-      }
-    },
-    isLoading,
-  };
+            try {
+                const result = await opts.mutation(...args);
+                return result;
+            } catch (e) {
+                throw e;
+            } finally {
+                setIsLoading(false);
+            }
+        },
+        isLoading,
+    };
 };
 
 const mutation = useMutation({
-  mutation: createUser,
+    mutation: createUser,
 });
 
-mutation.mutate({ name: "John Doe", email: "john@doe.com" });
+mutation.mutate({ name: 'John Doe', email: 'john@doe.com' });
 
 // @ts-expect-error email missing!
-mutation.mutate({ name: "John Doe" });
+mutation.mutate({ name: 'John Doe' });
 
 mutation.mutate(
-  {
-    name: "John Doe",
-    email: "john@doe.com",
-  },
-  {
-    throwOnError: true,
-    // @ts-expect-error extra prop
-    extra: "oh dear",
-  },
+    {
+        name: 'John Doe',
+        email: 'john@doe.com',
+    },
+    {
+        throwOnError: true,
+        // @ts-expect-error extra prop
+        extra: 'oh dear',
+    }
 );
 
 type test = [
-  Expect<Equal<typeof mutation.isLoading, boolean>>,
-  Expect<
-    Equal<
-      typeof mutation.mutate,
-      (
-        user: { name: string; email: string },
-        opts?: {
-          throwOnError?: boolean;
-        },
-      ) => Promise<{
-        id: string;
-        name: string;
-        email: string;
-      }>
+    Expect<Equal<typeof mutation.isLoading, boolean>>,
+    Expect<
+        Equal<
+            typeof mutation.mutate,
+            (
+                user: { name: string; email: string },
+                opts?: {
+                    throwOnError?: boolean;
+                }
+            ) => Promise<{
+                id: string;
+                name: string;
+                email: string;
+            }>
+        >
     >
-  >,
 ];
